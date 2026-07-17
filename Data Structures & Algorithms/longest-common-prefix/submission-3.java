3a8702e8d2806cb3c9c810fa1d2ee0214e04530b
SrajalJain <10srajaljain01@gmail.com>
2026-07-18 05:14:16 +0530

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        
+        String s =new String();
+        for(int i=0;i<strs[0].length();i++){
+            if(strs[0].charAt(i)==(strs[strs.length-1].charAt(i))){
+                s += strs[0].charAt(i);
+            }
+            else {
+                break;
+            }
+        }
+        
+        return s;
+    }
+}
